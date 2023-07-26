23c96ddafab496199d11304c9eb7e38c1f70fd5b
juh518 <ju_h518@naver.com>
2023-07-26 10:24:15 +0900

    [2W] 구글 홈페이지 제작 -  1 1. 디자인 수정

--- src/components/Main/MainContents.js
+++ src/components/Main/MainContents.js
@@ -6,20 +6,20 @@ const MainContents = () => {
     <section>
         <div class={styles.sectionContents}>
           <div>
-            <img src="http://placehold.it/112X112" alt="이미지 1" />
-            <img src="http://placehold.it/112X112" alt="이미지 2" />
-            <img src="http://placehold.it/112X112" alt="이미지 3" />
-            <img src="http://placehold.it/112X112" alt="이미지 4" />
-            <img src="http://placehold.it/112X112" alt="이미지 5" />
-            <img src="http://placehold.it/112X112" alt="이미지 6" />
+            <img src="http://placehold.it/80X80" alt="이미지 1" />
+            <img src="http://placehold.it/80X80" alt="이미지 2" />
+            <img src="http://placehold.it/80X80" alt="이미지 3" />
+            <img src="http://placehold.it/80X80" alt="이미지 4" />
+            <img src="http://placehold.it/80X80" alt="이미지 5" />
+            <img src="http://placehold.it/80X80" alt="이미지 6" />
           </div>
           <div>
-            <img src="http://placehold.it/112X112" alt="이미지 7" />
-            <img src="http://placehold.it/112X112" alt="이미지 8" />
-            <img src="http://placehold.it/112X112" alt="이미지 9" />
-            <img src="http://placehold.it/112X112" alt="이미지 10" />
-            <img src="http://placehold.it/112X112" alt="이미지 11" />
-            <img src="http://placehold.it/112X112" alt="이미지 12" /> 
+            <img src="http://placehold.it/80X80" alt="이미지 7" />
+            <img src="http://placehold.it/80X80" alt="이미지 8" />
+            <img src="http://placehold.it/80X80" alt="이미지 9" />
+            <img src="http://placehold.it/80X80" alt="이미지 10" />
+            <img src="http://placehold.it/80X80" alt="이미지 11" />
+            <img src="http://placehold.it/80X80" alt="이미지 12" /> 
           </div>
         </div>
   </section>
